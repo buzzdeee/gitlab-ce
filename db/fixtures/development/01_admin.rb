48a12d7af1b9b8ac56e9ae7f49779da3203fb2e7
Rémy Coutable <remy@rymai.me>
2017-10-17 13:09:49 +0200

    New .dedicated-no-docs-pull-cache-job and .no-db-job definitions
    This should speed up a bit some jobs.
    
    Also, use ruby:2.4-alpine image in the flaky-examples-check job
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- db/fixtures/development/01_admin.rb
+++ db/fixtures/development/01_admin.rb
@@ -1,5 +1,17 @@
 require './spec/support/sidekiq'
 
+# In the case where no ApplicationSetting record exists and the (Redis) cache is
+# cold, two ApplicationSetting records can be created in the same transaction
+# when creating a user because the following two methods trigger an
+# ApplicationSetting record creation:
+#   1. default_value_for(:external)
+#   2. after_initialize :set_projects_limit
+
+# That's why we eager-load the current application settings so that:
+#   1. An ApplicationSetting record is created (if needed) in the DB
+#   2. The (Redis) cache is warmed up
+Gitlab::CurrentSettings.current_application_settings
+
 Gitlab::Seeder.quiet do
   User.seed do |s|
     s.id = 1
