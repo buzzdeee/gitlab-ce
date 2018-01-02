5ffdaccce96fbc494d1af976d83c6d7daf82c31d
Simon Knox <psimyn@gmail.com>
2018-01-02 16:31:32 +1100

    start adding diff lines and diff_file backend

--- app/serializers/discussion_entity.rb
+++ app/serializers/discussion_entity.rb
@@ -15,4 +15,21 @@ class DiscussionEntity < Grape::Entity
   expose :resolve_with_issue_path do |discussion|
     new_project_issue_path(discussion.project, merge_request_to_resolve_discussions_of: discussion.noteable.iid, discussion_to_resolve: discussion.id)
   end
+
+  expose :diff_file, using: DiffFileEntity, if: -> (d, _) { defined? d.diff_file }
+
+  expose :diff_discussion?, as: :diff_discussion
+
+  expose :truncated_diff_lines, if: -> (d, _) { defined? d.diff_file } do |discussion|
+    options[:context].render_to_string(
+      partial: "projects/diffs/line",
+      collection: discussion.truncated_diff_lines,
+      as: :line,
+      locals: { diff_file: discussion.diff_file,
+        discussion_expanded: true,
+        plain: true },
+      layout: false,
+      formats: [:html]
+    )
+  end
 end
