ce24f2dda94cfd28297fca0cdf1f72ab832c7065
Tim Zallmann <tzallmann@gitlab.com>
2018-01-05 11:24:20 +0100

    Fixed Merge Request Changes Display

--- app/assets/javascripts/ide/components/repo_editor.vue
+++ app/assets/javascripts/ide/components/repo_editor.vue
@@ -37,22 +37,24 @@ export default {
       this.getRawFileData(this.activeFile)
         .then(() => {
           this.editor.createInstance(this.$refs.editor, this.$refs.diffEditor);
+        })
+        .then(() => {
+          this.setupEditor();
           this.setupViewMode();
         })
-        .then(() => this.setupEditor())
-        .catch((e) => {
+        .catch(e => {
           // flash('Error setting up monaco. Please try again.'));
-          console.log('Error in Monaco : ',e);
+          console.log('Error in Monaco : ', e);
         });
     },
     setupEditor() {
       if (!this.activeFile) return;
 
-      const model = this.editor.createModel(this.activeFile);
+      this.editorModel = this.editor.createModel(this.activeFile);
 
-      this.editor.attachModel(model);
+      this.editor.attachModel(this.editorModel);
 
-      model.onChange((m) => {
+      this.editorModel.onChange(m => {
         this.changeFileContent({
           file: this.activeFile,
           content: m.getValue(),
@@ -74,19 +76,15 @@ export default {
 
       // Handle File Language
       this.setFileLanguage({
-        fileLanguage: model.language,
+        fileLanguage: this.editorModel.language,
       });
 
       // Get File eol
       this.setFileEOL({
-        eol: model.eol,
+        eol: this.editorModel.eol,
       });
     },
-    selectViewMode(e) {
-      console.log('Selected View Mode : ',e);
-    },
     setupViewMode(selectedMode) {
-      
       if (selectedMode) this.setFileViewMode(selectedMode);
 
       this.$refs.editor.style.display = 'none';
@@ -94,14 +92,19 @@ export default {
 
       const choosenMode = selectedMode || this.activeFile.viewMode;
       console.log('SETUP VIEW MODE : ' + choosenMode);
-      if (choosenMode==='edit') {
+      if (choosenMode === 'edit') {
         this.$refs.editor.style.display = 'block';
       } else {
         this.$refs.diffEditor.style.display = 'block';
+        if (choosenMode === 'changes') {
+          this.editor.setDiffModel(this.editorModel, this.editorModel.getOriginalModel());
+        } else if (choosenMode === 'mrchanges') {
+          this.editor.setDiffModel(this.editorModel, this.editorModel.getTargetModel());
+        }
       }
 
       this.editor.updateDimensions();
-    }
+    },
   },
   watch: {
     activeFile(oldVal, newVal) {
@@ -122,21 +125,14 @@ export default {
     },
   },
   computed: {
-    ...mapGetters([
-      'activeFile',
-      'activeFileExtension',
-    ]),
-    ...mapState([
-      'leftPanelCollapsed',
-      'rightPanelCollapsed',
-      'panelResizing',
-    ]),
+    ...mapGetters(['activeFile', 'activeFileExtension']),
+    ...mapState(['leftPanelCollapsed', 'rightPanelCollapsed', 'panelResizing']),
     shouldHideEditor() {
       return this.activeFile.binary && !this.activeFile.raw;
     },
     currentViewMode() {
       return activeFile.viewMode;
-    }
+    },
   },
 };
 </script>
@@ -163,7 +159,7 @@ export default {
           <a
             href="javascript:void(0);"
             @click.prevent="setupViewMode('changes')">
-            Changes
+            Preview Changes
           </a>
         </li>
         <li
