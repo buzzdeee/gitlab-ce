4d41d1083a3eb65534daf8801210c4de890e4be5
Brett Walker <bwalker@gitlab.com>
2018-01-06 14:33:26 +0100

    can now call QA api with values for query string

--- qa/qa/runtime/api.rb
+++ qa/qa/runtime/api.rb
@@ -9,16 +9,32 @@ module QA
       # Example:
       #
       # get(:gitlab, api('/users', personal_access_token: 'something'))
+      # get(:gitlab, api('/users', personal_access_token: 'something'), username: value, ...)
       # get('http://gitlab.example', '/api/v4/users?private_token=something')
       #
       # In case of an address that is a symbol we will try to guess address
       # based on `Runtime::Scenario#something_address`.
-      def self.get(address, page)
-        Faraday.get API::Session.new(address, page).address
+      def self.get(address, page, *args)
+        page = self.add_query_values(page, args)
+        response = Faraday.get(API::Session.new(address, page).address)
+        json = response.status == 200 ? JSON.parse(response.body) : nil
+        return response, json
       end
 
       def self.version
         'v4'
+      end
+
+      def self.add_query_values(path, args)
+        if args.any?
+          query_string = Hash(*args).map { |key, value| "#{key}=#{value}" }.join('&')
+
+          if query_string
+            path << (path.index('?') ? '&' : '?')
+            path << query_string
+          end
+        end
+        path
       end
 
       class Session
