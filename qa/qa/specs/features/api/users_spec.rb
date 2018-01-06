65b94157b193dc6545c9718330a20702e0b2d6cb
Brett Walker <bwalker@gitlab.com>
2018-01-06 14:34:18 +0100

    initial user api spec for QA

---
+++ qa/qa/specs/features/api/users_spec.rb
@@ -0,0 +1,32 @@
+module QA
+  feature 'API users', :core do
+    include Support::ApiHelpers
+
+    before(:context) do
+      product = Factory::Resource::PersonalAccessToken.fabricate!
+      @access_token = product.factory.access_token
+    end
+
+    context "when authenticated" do
+      scenario 'get list of users' do
+        response, json = Runtime::API.get(:gitlab, api('/users', personal_access_token: @access_token))
+        expect(response).to have_gitlab_api_status(200)
+      end
+
+      scenario 'returns an empty response when an invalid `username` parameter is passed' do
+        response, json = Runtime::API.get(:gitlab, api('/users', personal_access_token: @access_token),
+                                          username: 'invalid')
+
+        expect(response).to have_gitlab_api_status(200)
+        expect(json).to be_an Array
+        expect(json.size).to eq(0)
+      end
+    end
+
+    scenario "returns authorization error when token is invalid" do
+      response, json = Runtime::API.get(:gitlab, api('/users', personal_access_token: 'invalid'))
+
+      expect(response).to have_gitlab_api_status(401)
+    end
+  end
+end
