d6b87dc7b3b817009f0dbfa45a43c74677adba37
Brett Walker <bwalker@gitlab.com>
2018-01-16 12:57:10 +0100

    implemented new view/selectors

--- qa/qa/page/menu/profile.rb
+++ qa/qa/page/menu/profile.rb
@@ -2,6 +2,12 @@ module QA
   module Page
     module Menu
       class Profile < Page::Base
+        view 'app/views/layouts/nav/sidebar/_profile.html.haml' do
+          element :access_token_link, 'link_to profile_personal_access_tokens_path'
+          element :access_token_title, 'Access Tokens'
+          element :top_level_items, '.sidebar-top-level-items'
+        end
+
         def click_access_tokens
           within_sidebar do
             click_link('Access Tokens')
--- qa/qa/page/profile/personal_access_tokens.rb
+++ qa/qa/page/profile/personal_access_tokens.rb
@@ -2,6 +2,16 @@ module QA
   module Page
     module Profile
       class PersonalAccessTokens < Page::Base
+        view 'app/views/shared/_personal_access_tokens_form.html.haml' do
+          element :personal_access_token_name_field, 'text_field :name'
+          element :create_token_button, 'submit "Create #{type} token"'
+          element :scopes_api_radios, "label :scopes"
+        end
+
+        view 'app/views/profiles/personal_access_tokens/index.html.haml' do
+          element :create_token_field, "text_field_tag 'created-personal-access-token'"
+        end
+
         def fill_token_name(name)
           fill_in 'personal_access_token_name', with: name
         end
